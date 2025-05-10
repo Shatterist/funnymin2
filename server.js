e2806850986686da78438f80c54e00f478fc6c29
Shatterist <migueljrgatdula@proton.me>
2025-05-10 16:20:00 +0800

    HTRFDCV

--- server.js
+++ server.js
@@ -32,8 +32,7 @@ const server = http.createServer(async function (request, response) {
             body += chunk;
         });
 
-        response.write(body)
-        return response.end()
+        return body
     }
     response.writeHead(200, {"Content-Type": "text/html"})
     response.end("funny min!!!!")
