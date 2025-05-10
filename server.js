ce23d5dc838a0b3d60d0e91a1454f5f68482fb11
Shatterist <migueljrgatdula@proton.me>
2025-05-10 15:05:23 +0800

    y

---
+++ server.js
@@ -0,0 +1,37 @@
+const http = require("http");
+const url = require("url");
+const fs = require("fs").promises;
+
+const port = process.env.PORT || 8080;
+
+async function getFile(path) {
+    try {
+        const data = await fs.readFile(path);
+        return data;
+    } catch (error) {
+        return null;
+    }
+}
+
+const server = http.createServer(async function (request, response) {
+    if (request.url === "/getbans") {
+        const data = await getFile("./bans.json")
+        if (!data) {
+             response.writeHead(404, {"Content-Type": "text/html"})
+             return response.end("Error, bans.json not found.")
+        }
+        response.writeHead(200, {"Content-Type": "application/json"})
+        response.write(data)
+        return response.end()
+    }
+    else if (request.url == "/ban" && request.method == "POST") {
+        response.writeHead(200, {"Content-Type": "text/html"})
+        return response.end("sure budy")
+    }
+    response.writeHead(200, {"Content-Type": "text/html"})
+    response.end("funny min!!!!")
+});
+
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
