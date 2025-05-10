f0277af45022a03a07430557593eb579afe1cbd8
Shatterist <migueljrgatdula@proton.me>
2025-05-10 15:54:30 +0800

    y

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const server = http.createServer(async function (request, response) {
         return response.end()
     }
     else if (request.url == "/ban" && request.method == "POST") {
-        return request
+        return response.end(request.data)
     }
     response.writeHead(200, {"Content-Type": "text/html"})
     response.end("funny min!!!!")
